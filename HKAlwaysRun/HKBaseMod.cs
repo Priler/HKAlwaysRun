7db0f582810a3ac98aaa73f92091f9319094ea76
Priler <priler96@gmail.com>
2025-09-25 05:44:08 +0500

    fg

---
+++ HKAlwaysRun/HKBaseMod.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("com.priler.hkalwaysrun", "HK Always Run", "1.0.0")]
+public class HKMod : BaseUnityPlugin
+{
+    internal new static ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo("Plugin loaded and initialized.");
+
+        Harmony.CreateAndPatchAll(typeof(HKMod), null);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(HeroController), "Move")]
+    private static void MovePrefix(HeroController __instance, float move_direction)
+    {
+        __instance.playerData.equippedCharm_37 = !__instance.cState.nearBench;
+    }
+}
